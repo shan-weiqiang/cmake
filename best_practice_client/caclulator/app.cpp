4937731f4a0f800427b62e079f41c6494adf48c9
shanweiqiang <schmessi@163.com>
2024-09-18 12:56:29 +0800

    add client

---
+++ best_practice_client/caclulator/app.cpp
@@ -0,0 +1,5 @@
+#include "caculator/caculator.h"
+#include <iostream>
+int main(){
+    std::cout << Caculator().divide(1.2, 3.4) << std::endl;
+}
---
+++ best_practice_client/math/app.cpp
@@ -0,0 +1,5 @@
+#include "divide/divide.h"
+#include <iostream>
+int main(){
+    std::cout << math::divide(1.2, 3.4) << std::endl;
+}
