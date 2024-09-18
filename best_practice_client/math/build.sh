f7d9be1c2106e793efee1fcbaca3780a70aaa1c0
shanweiqiang <schmessi@163.com>
2024-09-18 14:39:57 +0800

    add json

---
+++ best_practice_client/math/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+rm -rf build
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX=./build/install -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build
+cmake --install build
--- best_practice_lib/src/multi/include/multi/multi.h
+++ best_practice_lib/src/multi/include/multi/multi.h
@@ -1,3 +1,5 @@
+// Make this unit depend on json
+#include "json.hpp"
 namespace math {
 float multi(float, float);
 }
